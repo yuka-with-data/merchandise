bec7280dc0eb5029cb9c2d70f925c6a1a4055a6e
Yuka <90426808+yuka-with-data@users.noreply.github.com>
2022-07-31 00:08:06 -0700

    First Commit
    Adding source code files (R and Python) and Carbon generated images

---
+++ yuka_with_R.R
@@ -0,0 +1,13 @@
+# my life be like
+x <- 1
+repeat{
+  print(c("eat",
+          "drink coffee",
+          "Yuka_With_DATA",
+          "sleep"))  
+  x <- x + 1
+  if (x == 365){
+    print("take a break");
+    break
+  }
+}
---
+++ yuka_with_python.py
@@ -0,0 +1,11 @@
+day_in_life = ["eat",
+"drink coffee",
+"Yuka_with_Data",
+"sleep"] 
+i=1
+while True:
+    print(day_in_life)
+    if i >= 365:
+        print("take a break")
+        break
+    i += 1
